ace48fa6298931ef92f2086b2594961a9aa0d531
Miller MA <this.is.millerma@gmail.com>
2022-02-07 23:33:33 -0700

    bug fixed

--- httpclient.py
+++ httpclient.py
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# Ruilin Ma 2021
 
 # Do not use urllib's HTTP GET and POST mechanisms.
 # Write your own HTTP GET and POST
